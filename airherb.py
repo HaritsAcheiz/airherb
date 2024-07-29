92b2a1f148132ded6f7411bdc8901235cd761d26
Muhammad Harits R <harits.muhammad.only@gmail.com>
2024-07-29 14:39:59 +0700

    Script Work normally, next store data into database.

--- airherb.py
+++ airherb.py
@@ -4,10 +4,12 @@ from selectolax.parser import HTMLParser
 from dataclasses import dataclass, field
 from typing import List
 from random import choice
+from urllib.parse import urljoin
 
 @dataclass
 class AirHerb:
     useragent: str = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    base_url: str = 'https://www.iherb.com'
 
     # useragent: List[str] = field(default_factory=lambda: [
     #     'Mozilla/5.0 (Wayland; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.137 Safari/537.36 Ubuntu/22.04 (5.0.2497.35-1) Vivaldi/5.0.2497.35',
@@ -23,68 +25,56 @@ class AirHerb:
     #                                                     '154.38.156.188:8800'])
 
 
-    async def fetch(self, client, url, proxies, headers):
-        async with client.get(url, proxy=proxies, headers=headers) as r:
-            if r.status != 200:
-                r.raise_for_status()
-            return await r.text()
+    async def fetch(self, client, url):
+        print(f'Fetching {url}...', end='')
+
+        headers = {
+            'user-agent': self.useragent
+        }
+
+        async with client.get(url, headers=headers) as response:
+            if response.status != 200:
+                response.raise_for_status()
+            result = await response.text()
+
+        print('Completed')
+
+        return result
 
 
     async def extract(self):
         urls = []
         for page in range(1, 3):
-            url = f'https://www.iherb.com/specials?sr=3&p={page}'
+            url = urljoin(self.base_url, f'/specials?sr=3&p={page}')
             urls.append(url)
 
         async with aiohttp.ClientSession() as client:
-            tasks = []
-
-            for url in urls:
-                # proxy = choice(self.proxies)
-                # proxies = f"http://{proxy}"
-
-                ua = choice(self.useragent)
-
-                headers = {
-                    'user-agent': self.useragent
-                }
-
-                task = asyncio.create_task(self.fetch(
-                    client=client,
-                    url=url,
-                    # proxies=proxies,
-                    headers=headers))
-
-                tasks.append(task)
-
+            tasks = [self.fetch(client, url=url) for url in urls]
             results = await asyncio.gather(*tasks)
 
         return results
 
 
-    def get_detail_links(self, htmls):
-        results = []
-        for html in htmls:
-            tree = HTMLParser(html)
+    def get_detail_links(self, responses):
+        products = []
+        for response in responses:
+            tree = HTMLParser(response)
             product_list = tree.css('div.products.product-cells.clearfix > div')
-            products = []
             for product in product_list:
-                product_temp = {'product_id': '', 'price': '', 'out_of_stock': 'True', 'link': ''}
+                product_temp = dict()
                 product_temp['product_id'] = product.css_first('a.absolute-link.product-link').attributes.get('data-product-id', '')
                 product_temp['price'] = product.css_first('a.absolute-link.product-link').attributes.get('data-ga-discount-price', '')
                 product_temp['out_of_stock'] = product.css_first('a.absolute-link.product-link').attributes.get('data-ga-is-out-of-stock', 'True')
                 product_temp['link'] = product.css_first('a.absolute-link.product-link').attributes.get('href', '')
                 products.append(product_temp)
 
-            results.extend(products)
-
-        return results
+        return products
 
 
     def run(self):
-        htmls = asyncio.run(self.extract())
-        detail_links = self.get_detail_links(htmls)
-        print(detail_links)
+        responses = asyncio.run(self.extract())
+        detail_links = self.get_detail_links(responses)
+
 
 if __name__ == '__main__':
     scraper = AirHerb()
